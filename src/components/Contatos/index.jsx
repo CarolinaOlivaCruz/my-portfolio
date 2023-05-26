08b8805d675b1519e64bcc96c56c903a7c52a7a6
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-05-26 12:39:56 -0300

    ref: links target blank

--- src/components/Contatos/index.jsx
+++ src/components/Contatos/index.jsx
@@ -11,11 +11,11 @@ const Contato = () => {
       <ul>
         <li>
           <img src={github} alt="" />
-          <a href="https://github.com/CarolinaOlivaCruz">Github</a>
+          <a href="https://github.com/CarolinaOlivaCruz" target="_blank" rel="noopener noreferrer">Github</a>
         </li>
         <li>
           <img src={linkedin} alt="" />
-          <a href="https://linkedin.com/in/carolina-oliva-cruz">Linkedin</a>
+          <a href="https://linkedin.com/in/carolina-oliva-cruz" target="_blank" rel="noopener noreferrer">Linkedin</a>
         </li>
         <li>
           <img src={gmail} alt="" />
